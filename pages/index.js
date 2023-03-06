c565307fac67406697725d2cf7f865a37776f202
Theo <theotournn@gmail.com>
2023-03-06 01:09:36 -0300

    fixed images

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home(props) {
   return (
     <>
       <Head>
-        <title>Valorant Agents</title>
+        <title>Valorant Wiki</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.bg}>
@@ -16,43 +16,48 @@ export default function Home(props) {
         <div className="container">
           <div className="d-flex flex-wrap">
             <div
-              className="card card-body text-white bg-dark"
-              style={{ margin: "1rem" }}
+              className="card card-body text-white bg-dark d-flex flex-column justify-content-between align-items-center"
+              style={{ margin: "1rem", flex: 1, maxheight: "35rem" }}
             >
-              <img
-                src="https://playvalorant.com/static/agents-group-31d7ce5a3637e45d8b25d2fd03159e6c.png"
-                width="400rem"
-                height="400rem"
-                className="card-img-top"
-              />
-              <Link href="/agentspage">
-                <div className="text-center">
+              <div className="d-flex justify-content-center align-items-center flex-grow-1">
+                <img
+                  src="https://playvalorant.com/static/agents-group-31d7ce5a3637e45d8b25d2fd03159e6c.png"
+                  className="card-img-top"
+                  style={{ objectFit: "contain", height: "100%" }}
+                />
+              </div>
+              <div className="text-center">
+                <Link href="/agentspage">
                   <button
                     className="btn btn-dark"
                     style={{ fontWeight: "bold" }}
                   >
                     Agents
                   </button>
-                </div>
-              </Link>
+                </Link>
+              </div>
             </div>
             <div
-              className="card card-body text-white bg-dark"
-              style={{ margin: "1rem" }}
+              className="card card-body text-white bg-dark d-flex flex-column justify-content-between align-items-center"
+              style={{ margin: "1rem", flex: 1, maxheight: "35rem" }}
             >
-              <img
-                src="https://vgraphs.com/images/weapons/skins/valorant-weapon-skin-standard-vandal.png"
-                width="400rem"
-                height="400rem"
-                className="card-img-top"
-              />
-              <Link href="/gunspage">
+              <div className="d-flex justify-content-center align-items-center flex-grow-1">
+                <img
+                  src="https://media.valorant-api.com/weapons/9c82e19d-4575-0200-1a81-3eacf00cf872/displayicon.png"
+                  className="card-img-top"
+                  style={{ objectFit: "contain", height: "100%" }}
+                />
+              </div>
               <div className="text-center">
-                <button className="btn btn-dark" style={{ fontWeight: "bold" }}>
-                  Guns
-                </button>
-                </div>
-              </Link>
+                <Link href="/gunspage">
+                  <button
+                    className="btn btn-dark"
+                    style={{ fontWeight: "bold" }}
+                  >
+                    Guns
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
